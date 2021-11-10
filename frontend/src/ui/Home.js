d0faf892b11bb2cc991912f6d64c5b6acec775d3
*Jamie <jdavis218@cnm.edu>
2021-11-10 14:35:49 -0600

    added useState and modals

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,10 +1,16 @@
 import React from "react"
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { useState } from 'react'
+import { Container, Row, Col, Button, Modal, } from 'react-bootstrap'
 import "./homeButtons.css"
 
 export const Home = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <>
+
+  <>
 
       {/*Website Name*/}
 
@@ -26,16 +32,60 @@ export const Home = () => {
       <Container fluid>
         <Row>
           <Col md={4} className="d-grid gap-2 mx-auto">
-            <Button className="target mx-5">Create Group</Button>
+            <Button className="target mx-5" onClick={handleShow}>Create Group</Button>
 
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Create Group</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Col>
-          <Col md={4} className="d-grid gap-2 mx-auto">
-            <Button className="target mx-5">Join Group</Button>
 
+          <Col md={4} className="d-grid gap-2 mx-auto">
+            <Button className="target mx-5" onClick={handleShow}>Join Group</Button>
+
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Join Group</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Col>
-          <Col md={4} className="d-grid gap-2 mx-auto">
-            <Button className="target mx-5">Flying Solo</Button>
 
+          <Col md={4} className="d-grid gap-2 mx-auto">
+            <Button className="target mx-5" onClick={handleShow}>Flying Solo</Button>
+
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Flying Solo</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </Col>
         </Row>
       </Container>
