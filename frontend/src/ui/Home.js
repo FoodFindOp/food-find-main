d81fbd1786daf96b1386862349a833d45f2aca30
*Jamie <jdavis218@cnm.edu>
2021-11-10 17:03:28 -0600

    ran into issues, may need to restructure wireframes

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { useState } from 'react'
-import { Container, Row, Col, Button, Modal, } from 'react-bootstrap'
+import { Container, Row, Col, Button, Modal, Form } from 'react-bootstrap'
 import "./homeButtons.css"
 
 export const Home = () => {
@@ -8,6 +8,18 @@ export const Home = () => {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
+
+  const [show3, setShow3] = useState(false);
+
+  const handleClose3 = () => setShow3(false);
+  const handleShow3 = () => setShow3(true);
+
+
   return (
 
   <>
@@ -38,11 +50,22 @@ export const Home = () => {
               <Modal.Header closeButton>
                 <Modal.Title>Create Group</Modal.Title>
               </Modal.Header>
-              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Body>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicText">
+                    <Form.Label>Group Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Group Name" /></Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Password" /></Form.Group>
+
+
+                </Form>
+
+
+
+              </Modal.Body>
               <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
                 <Button variant="primary" onClick={handleClose}>
                   Save Changes
                 </Button>
@@ -51,18 +74,15 @@ export const Home = () => {
           </Col>
 
           <Col md={4} className="d-grid gap-2 mx-auto">
-            <Button className="target mx-5" onClick={handleShow}>Join Group</Button>
+            <Button className="target mx-5" onClick={handleShow2}>Join Group</Button>
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show2} onHide={handleClose2}>
               <Modal.Header closeButton>
                 <Modal.Title>Join Group</Modal.Title>
               </Modal.Header>
               <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
               <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" onClick={handleClose2}>
                   Save Changes
                 </Button>
               </Modal.Footer>
@@ -70,18 +90,15 @@ export const Home = () => {
           </Col>
 
           <Col md={4} className="d-grid gap-2 mx-auto">
-            <Button className="target mx-5" onClick={handleShow}>Flying Solo</Button>
+            <Button className="target mx-5" onClick={handleShow3}>Flying Solo</Button>
 
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show3} onHide={handleClose3}>
               <Modal.Header closeButton>
                 <Modal.Title>Flying Solo</Modal.Title>
               </Modal.Header>
               <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
               <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" onClick={handleClose3}>
                   Save Changes
                 </Button>
               </Modal.Footer>
@@ -89,9 +106,6 @@ export const Home = () => {
           </Col>
         </Row>
       </Container>
-
-
-
     </>
   )
 }
