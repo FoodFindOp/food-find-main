9739518088828a5f0eeb015dcc43940154680f23
*Jamie <jdavis218@cnm.edu>
2021-11-10 11:28:41 -0600

    outline for home page started

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,9 +1,46 @@
 import React from "react"
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 export const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+
+      {/*Website Name*/}
+
+      <Container>
+        <Row>
+          <Col className="py-5 d-flex justify-content-center">
+            <h1>Restaurant Tinder</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="py-5 d-flex justify-content-center">
+            <p>What would you like to do?</p>
+          </Col>
+        </Row>
+      </Container>
+
+      {/*Option Containers*/}
+
+      <Container>
+        <Row>
+          <Col className="d-grid gap-2 col-md-3 mx-auto">
+            <Button size="lg">Create Group</Button>
+
+          </Col>
+          <Col className="d-grid gap-2 col-md-3 mx-auto">
+            <Button>Join Group</Button>
+
+          </Col>
+          <Col className="d-grid gap-2 col-md-3 mx-auto">
+            <Button>Flying Solo</Button>
+
+          </Col>
+        </Row>
+      </Container>
+
+
+
     </>
   )
 }
