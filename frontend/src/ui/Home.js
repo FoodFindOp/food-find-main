eb01d6b6571bf3c4891745265d0c3a2728a28cd9
*Jamie <jdavis218@cnm.edu>
2021-11-15 11:48:40 -0600

    burning home page ticket

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,18 +1,15 @@
 import React from "react"
 import { useState } from 'react'
 import { Container, Row, Col, Button, Modal, Form } from 'react-bootstrap'
-import './HomeStyle.css'
-
+import background from './Background.module.css'
+import buttons from './Buttons.module.css'
+import './Bg.css'
 export const Home = () => {
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const [show2, setShow2] = useState(false);
-
-  const handleClose2 = () => setShow2(false);
-  const handleShow2 = () => setShow2(true);
 
   const [show3, setShow3] = useState(false);
 
@@ -27,6 +24,8 @@ export const Home = () => {
   <>
 
       {/*Website Name*/}
+
+
 
       <Container>
         <Row>
@@ -45,8 +44,11 @@ export const Home = () => {
 
       <Container fluid>
         <Row>
+
+          {/*Create group*/}
+
           <Col md={6} className="d-grid gap-2 mx-auto">
-            <Button className="target" onClick={handleShow}>Create Group</Button>
+            <Button className={buttons.height} onClick={handleShow}>Create Group</Button>
 
             {/*Create group modal*/}
 
@@ -55,6 +57,9 @@ export const Home = () => {
                 <Modal.Title>Create Group</Modal.Title>
               </Modal.Header>
               <Modal.Body>
+
+                {/*Create group form*/}
+
                 <Form>
                   <Form.Group className="mb-3" controlId="formGroupAddress">
                     <Form.Label>Location</Form.Label>
@@ -80,16 +85,21 @@ export const Home = () => {
             </Modal>
           </Col>
 
-          {/*Flying Solo*/}
+          {/*Flying solo*/}
 
           <Col md={6} className="d-grid gap-2 mx-auto">
-            <Button className="target" onClick={handleShow3}>Flying Solo</Button>
+            <Button className={buttons.height} onClick={handleShow3}>Flying Solo</Button>
+
+            {/*Flying solo modal*/}
 
             <Modal show={show3} onHide={handleClose3}>
               <Modal.Header closeButton>
                 <Modal.Title>Flying Solo</Modal.Title>
               </Modal.Header>
               <Modal.Body>
+
+                {/*Flying solo form*/}
+
                 <Form>
                   <Form.Group className="mb-3" controlId="formGroupAddress">
                     <Form.Label>Location</Form.Label>
@@ -105,6 +115,7 @@ export const Home = () => {
                     </Form.Select>
                   </Form.Group>
                 </Form>
+
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="primary" onClick={handleClose}>
@@ -113,6 +124,7 @@ export const Home = () => {
               </Modal.Footer>
             </Modal>
           </Col>
+
         </Row>
       </Container>
   </>
