c7f0bfbbb4f5752e1d5b0f8b97c6d98650233a75
Jesslyn Bjork <jbjork@cnm.edu>
2021-11-16 16:06:14 -0600

    got js to work

--- frontend/src/ui/ProfilePage.js
+++ frontend/src/ui/ProfilePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Form} from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 
 export const ProfilePage = () => {
 
@@ -7,52 +7,57 @@ export const ProfilePage = () => {
         <>
             <h1 className="d-flex justify-content-center">Create Your Profile!</h1>
 
-            <form>
-                <div className="form-group">
-                    <div className="col-sm-4">
+            <Form>
+                <Container className="form-group">
+                    <Row className="col-sm-4">
                         <label htmlFor="firstname">First Name</label>
-                        <input type="email" className="form-control" id="firstname" placeholder="name@example.com">
-                    </div>
-                </div>
+                        <input type="email" className="form-control" id="firstname" placeholder="name@example.com"/>
+                    </Row>
+                </Container>
 
 
-                <div className="form-group">
-                    <div className="col-sm-4">
+                <Container className="form-group">
+                    <Row className="col-sm-4">
                         <label htmlFor="lastname">Last Name</label>
-                        <input type="email" className="form-control" id="lastname" placeholder="name@example.com">
-                    </div>
-                </div>
+                        <input type="email" className="form-control" id="lastname" placeholder="name@example.com"/>
+                 </Row>
+            </Container>
 
 
-                <div className="form-group">
-                    <div className="form-group">
-                        <div className="col-sm-4">
+                <Container className="form-group">
+                    <Row className="form-group">
+                        <Col className="col-sm-4">
                             <label htmlFor="email">Email address</label>
                             <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
-                                   placeholder="Enter email">
+                                   placeholder="Enter email"/>
+                        </Col>
+                    </Row>
+                </Container>
 
-                        </div>
-                        <div className="form-group">
-                            <div className="form-group">
-                                <div className="col-sm-4">
+
+                        <Container className="form-group">
+                            <Row className="form-group">
+                                <Col className="col-sm-4">
                                     <label htmlFor="password">Password</label>
                                     <input type="password" className="form-control" id="password"
                                            placeholder="Password"/>
-                                </div>
+                                </Col>
+                            </Row>
+                        </Container>
 
-                                <div className="form-group">
-                                    <div className="form-group">
-                                        <div className="col-sm-4">
+                                <Container className="form-group">
+                                    <Row className="form-group">
+                                        <Col className="col-sm-4">
                                             <label htmlFor="confirmpassword">Confirm Password</label>
                                             <input type="password" className="form-control" id="confirmpassword"
                                                    placeholder="Password"/>
                                             <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </>
-    )
+                                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Form>
+                        </>
+                    )
 }
 
 
