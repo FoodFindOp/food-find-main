0295bcd0bba1dcf34f3a89ce88daf21642e0fdd5
*Jamie <jdavis218@cnm.edu>
2021-12-11 10:20:50 -0600

    sign in burned

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,8 @@
 import profile from "./profile"
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import auth from "./auth"
 
-const reducer =  combineReducers({profile})
+const reducer =  combineReducers({profile, auth})
+
 
 export const store = configureStore({reducer})
